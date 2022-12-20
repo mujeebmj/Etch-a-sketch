2d473bee4db7ae1de6029aa4c9b94c6e01cec00a
mujeeb ahmad <sayedmujeeb786786@gmail.com>
2022-12-20 17:14:11 +0530

    some changes

---
+++ script.js
@@ -0,0 +1,12 @@
+const colorMode = document.querySelector("#colorMode") 
+const rainbowMode = document.querySelector("#colorMode") 
+const erase = document.querySelector("#colorMode") 
+const clear = document.querySelector("#colorMode") 
+const slider = document.querySelector(".slider")
+const etchingPad = document.querySelector(".etchingPad")
+
+
+
+function gridCreations (){
+    // etchingPad.style.gridCreations
+}
