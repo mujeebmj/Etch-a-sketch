7659e2d55cca171197edfd8e0ef3a5f692d87779
mujeeb ahmad <sayedmujeeb786786@gmail.com>
2022-12-22 00:36:46 +0530

    create a functotion of gridcreation

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-const colorMode = document.querySelector("#colorMode") 
-const rainbowMode = document.querySelector("#colorMode") 
-const erase = document.querySelector("#colorMode") 
-const clear = document.querySelector("#colorMode") 
-const slider = document.querySelector(".slider")
-const rangeValue = document.querySelector("#rangeValue")
-const etchingPad = document.querySelector(".etchingPad")
+let colorMode = document.querySelector("#colorMode") ;
+let rainbowMode = document.querySelector("#rainbowMode") ;
+let erase = document.querySelector("#erase") ;
+let clear = document.querySelector("#clear") ;
+let etchingPad = document.querySelector(".etchingPad") ;
+
+document.addEventListener("DOMContentLoaded" , letsStartEtching(16)) ;
 
 
-function gridCreations (){
+
+function letsStartEtching(size) {
+
+let squares = etchingPad.querySelectorAll('div')
+    squares.forEach((squares)  => squares.remove())  
+    
+etchingPad.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+etchingPad.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+let amount = size * size;
+
+for(let i = 0 ; i < amount ; i++){
+
+    let square = document.createElement('div') ;
+    square.style.backgroundColor = 'white' ;
+    square.style.border = '1px solid gray' ;
+    etchingPad.appendChild(square)         //we also use this // etchingPad.insertAdjacentElement(("beforeend") , square) 
+}
 
 }
+
+
+
+function  setGridSize (){
+
+    let inputValue = document.querySelector(".input").value ;
+
+    if(inputValue < 101 && inputValue > 1){
+
+    letsStartEtching(inputValue);
+
+}   else{
+
+    let error = document.querySelector('#errorMassage');
+    error.textContent = "Size Must Be Between 2 to 100"
+
+}
+}
+
+// setGridSize()
